1129bce299d574c22306e778faaf5dedd9c49847
rohayem <rohayemmahfouz123456@gmail.com>
2026-08-22 11:04:33 +0300

    Delete NewTasks/Task2 directory

--- NewTasks/Task2/LIB/BIT_MATH.h
+++
@@ -1,9 +0,0 @@
-#ifndef _BIT_MATH_H_
-#define _BIT_MATH_H_
-
-#define SetBit(reg, bitno)         ((reg) |=  (1 << (bitno)))   
-#define ClearBit(reg, bitno)       ((reg) &= ~(1 << (bitno)))   
-#define ToggleBit(reg, bitno)      ((reg) ^=  (1 << (bitno)))   
-#define ReadBit(reg, bitno)        (((reg) >> (bitno)) & 1)     
-
-#endif
